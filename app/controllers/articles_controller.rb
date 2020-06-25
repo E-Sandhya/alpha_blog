8b09fb5e44a9fe3d817275e3739552dff1c3d157
E-Sandhya <sandhya.e@etvbharat.com>
2020-06-25 14:37:21 +0530

    created new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,23 @@ class ArticlesController < ApplicationController
     
     
     def show
-        byebug
+        #byebug
         @article = Article.find(params[:id ])  
     end
     def index
         @articles = Article.all
+    end 
+
+    
+
+    def new
+        
     end
-end 
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render plain: @article
+        @article.save
+        redirect_to  @article
+    end
+end                
