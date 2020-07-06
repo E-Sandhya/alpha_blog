7533ad7c496b8ed005e60ff3753c38efb7e6304e
E-Sandhya <sandhya.e@etvbharat.com>
2020-07-06 17:26:12 +0530

    added gravatar profile and index to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles
   end
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -30,6 +33,7 @@ class UsersController < ApplicationController
     end
 
   end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,11 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size} "
     image_tag(gravatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
   end
+  def users_profile(user)
+  
+    user.username.itself[0,1].to_s.upcase + user.username.match(/(?<=\s)./).to_s.upcase
+
+
+  end
 
 end
