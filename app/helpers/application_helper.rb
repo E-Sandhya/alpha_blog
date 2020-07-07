4c5ed8d119d0ec4f7a757d0c073139ddf2e5914e
E-Sandhya <sandhya.e@etvbharat.com>
2020-07-07 12:52:44 +0530

    default gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,10 @@ module ApplicationHelper
     email_address= user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
-    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size} "
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}?f=http://homepages.cae.wisc.edu/~ece533/images/airplane.png"
     image_tag(gravatar_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
   end
+  
   def users_profile(user)
   
     user.username.itself[0,1].to_s.upcase + user.username.match(/(?<=\s)./).to_s.upcase
