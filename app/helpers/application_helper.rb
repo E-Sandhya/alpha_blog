dfdface991b5ca5ad9b0736d00a4255da7c132ef
E-Sandhya <sandhya.e@etvbharat.com>
2020-07-14 17:05:15 +0530

    cleaned up layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,12 +12,17 @@ module ApplicationHelper
   
   def gravatar_for(user,options = { size: 80 })
     # byebug
-    default_url = "https://i.pinimg.com/originals/89/ed/c2/89edc2890c3d14a4ed3bbaab9be06a28.gif"
+    params = {
+      :a => user.username.itself[0,1].to_s.upcase + user.username.match(/(?<=\s)./).to_s.upcase
+    }
+    size = options[:size]
     user_path = user.username.itself[0,1].to_s.upcase + user.username.match(/(?<=\s)./).to_s.upcase
-
+    default_url = "http://www.lse.ac.uk/International-Inequalities/Assets/Images/BlankImage.jpg?h=60&w=60"
+    
+    # default_url = "http://via.placeholder.com/150/000000/FFFFFF/?text=".concat(user_path)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    gravatar_url= "http://gravatar.com/avatar/#{gravatar_id}.png?s=180&d=#{CGI.escape(default_url)}"
-    image_tag(gravatar_url, alt:user_path, class: "bg1 rounded-circle shadow mx-auto d-block")
+    gravatar_url= "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    image_tag(gravatar_url, alt:user_path, html: { style: "height: 60px;, width:60px;"}, class: "bg1 rounded-circle shadow mx-auto d-block")
     
     
   
